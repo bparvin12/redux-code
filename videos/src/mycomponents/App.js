214b7b381e5c22f05890d3570344c448851a565a
BijanParvin <bparvin12@gmail.com>
2020-01-29 11:06:52 -0800

    finished section 11. videos project

---
+++ videos/src/mycomponents/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+import youtube from "../myapis/youtube";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+
+class App extends React.Component {
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
+
+  componentDidMount() {
+      this.onTermSubmit('soccer')
+  }
+
+  onTermSubmit = async term => {
+    console.log(term);
+    const response = await youtube.get("/search", {
+      params: {
+        q: term
+      }
+    });
+    console.log(response.data.items);
+    this.setState({ 
+        videos: response.data.items,
+        selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    console.log("From the App", video);
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <div className="ui grid">
+          <div className='ui row'>
+              <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo} />
+              </div>
+            <div className="five wide column">
+                <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ videos/src/mycomponents/VideoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import VideoItem from './VideoItem';
+
+const VideoList = ({ videos, onVideoSelect }) => {
+    const renderedList = videos.map((video) => {
+        return (
+            <VideoItem 
+                video={video} 
+                key={video.id.videoId} 
+                onVideoSelect={onVideoSelect}
+            />
+        );
+    });
+    //props.videos
+    return (
+        <div className='ui relaxed divided list'>
+            {renderedList}
+        </div>
+    );
+};
+
+export default VideoList;
